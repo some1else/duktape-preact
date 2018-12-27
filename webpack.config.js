406d780b89828ee2b7cc46dcf425df7134cb7975
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2018-12-27 12:12:23 +0100

    Reconfigure build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path")
 
 module.exports = {
 	entry: {
-		browser: [
-			'./src/index.js',
-		],
+		browser: ["./src/index.js"],
 		duktapeHTML: [
-			'./src/polyfills.js',
-			'./src/index.duktape.string.js',
-			'./src/polyfills/eventLoop.js',
+			"./src/polyfills.js",
+			"./src/index.duktape.string.js",
+			"./src/polyfills/eventLoop.js"
 		],
 		duktapeJSON: [
-			'./src/polyfills.js',
-			'./src/index.duktape.json.js',
-			'./src/polyfills/eventLoop.js',
-		],
+			"./src/polyfills.js",
+			"./src/index.duktape.json.js",
+			"./src/polyfills/eventLoop.js"
+		]
 	},
 	module: {
 		rules: [
-			{ test: /\.js$/, use: { loader: 'babel-loader', options: {
-          presets: 
-          [ '@babel/preset-env' ]
-        } } },
+			{
+				test: /\.js$/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
 			{ test: /\.html$/, use: "html-loader" }
 		]
 	},
 	plugins: [
-    new HtmlWebPackPlugin({
-	      template: path.resolve(__dirname, 'src', 'index.html'),
-	      chunks: ['browser']
-    })
-  ]
-};
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			chunks: ["browser"]
+		})
+	]
+}
