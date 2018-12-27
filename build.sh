80781511ff58a5277bb81ed1e533fca6d5e459df
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2018-12-27 12:20:05 +0100

    Echo rendered markup at end of build

--- build.sh
+++ build.sh
@@ -8,3 +8,15 @@ node node_modules/webpack/bin/webpack.js --mode development
 ./bin/duk dist/duktapeJSON.js > dist/render.json
 
 cat dist/render.json | ./bin/jsoncbor -e > dist/render.cbor
+
+echo
+echo HTML:
+cat dist/render.html
+
+echo 
+echo JSON:
+cat dist/render.json
+
+echo
+echo CBOR:
+cat dist/render.cbor
