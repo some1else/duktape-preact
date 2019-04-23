02880baa21e16c2146aabc45410bc3471a8f8f26
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2019-04-23 11:13:44 +0200

    Update array-includes.js

--- src/polyfills/array-includes.js
+++ src/polyfills/array-includes.js
@@ -1,8 +1,5 @@
-if (typeof Array.prototype.includes !== 'function') {
-
-	// let Symbol = require('es6-symbol')
-
-	const includes = require('array-includes');
+if (typeof Array.prototype.includes !== "function") {
+	const includes = require("array-includes")
 
 	Array.prototype.includes = (...items) => {
 		return includes(this, ...items)
